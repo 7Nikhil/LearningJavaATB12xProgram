4cb6bce6cf254b3053be7453a4e4d6bde398a995
NikhilNandwana <nandwana.nikhil91@gmail.com>
2025-05-17 21:20:37 +0530

    Initial commit

---
+++ src/ex_06_Operators/NestedTernary.java
@@ -0,0 +1,11 @@
+package ex_06_Operators;
+
+public class NestedTernary {
+
+    public static void main(String [] args)
+    {
+        int x=10,y=1,z=18;
+
+        System.out.println(x>y?(z>x?"z is greator":"x is greater"):"y is greater");
+    }
+}
---
+++ src/ex_06_Operators/OddEvenTask.java
@@ -0,0 +1,13 @@
+package ex_06_Operators;
+
+import java.sql.SQLOutput;
+
+public class OddEvenTask
+{
+    public static void main(String [] args){
+
+        int x=11;
+        System.out.println(x%2==0? "Number is even": "Number is odd");
+
+}
+}
